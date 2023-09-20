fa562c885ae8a48fc823f48db3d1efdb8a1384bc
Hans Loedolff <hans.loedolff@gmail.com>
2023-09-19 22:21:22 -0700

    add seed data to the database table

--- server/db/db.sql
+++ server/db/db.sql
@@ -1,5 +1,14 @@
+
+drop table sightings;
+
 CREATE TABLE sightings (
     id serial PRIMARY KEY,
     nickname TEXT,
     location TEXT
 );
+
+insert into sightings(nickname, location) values ('flaffie', 'back yard');
+insert into sightings(nickname, location) values ('giant', 'forrest');
+insert into sightings(nickname, location) values ('tiny', 'by the river');
+
+select * from sightings;
