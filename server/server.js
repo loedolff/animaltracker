7c3a44d511607dd17d38a0919db00b4ee8e11628
Hans Loedolff <hans.loedolff@gmail.com>
2023-09-19 22:38:37 -0700

    add GET api/sightings

--- server/server.js
+++ server/server.js
@@ -5,6 +5,15 @@ import express from 'express'
 const app = express()
 const port = 3000
 
+app.get('/api/sightings', async (req, res) => {
+  try {
+    const users = await db.query('SELECT * FROM sightings');
+    res.send(users);
+  } catch (e) {
+    return res.status(400).json({ e });
+  }
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
