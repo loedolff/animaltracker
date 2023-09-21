8d6b8b170a7ac44ec018dd2d1445a8204309f53d
Hans Loedolff <hloedolff@indeed.com>
2023-09-21 13:48:29 -0700

    add species table

--- server/db/db.sql
+++ server/db/db.sql
@@ -1,8 +1,32 @@
 
-drop table sightings;
+DROP TABLE species;
+DROP TABLE sightings;
+
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    common_name TEXT,
+    scientific_name TEXT,
+    estimated_living INTEGER,
+    conservation_status char(2),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);  
+
+INSERT INTO species (
+    common_name,
+    scientific_name, 
+    estimated_living, 
+    conservation_status) 
+VALUES (
+    'Giant Panda', 
+    'Ailuropoda melanoleuca', 
+    1864, 
+    'EN'
+);
+
+select * from species;
 
 CREATE TABLE sightings (
-    id serial PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     nickname TEXT,
     location TEXT
 );
@@ -12,3 +36,4 @@ insert into sightings(nickname, location) values ('giant', 'forrest');
 insert into sightings(nickname, location) values ('tiny', 'by the river');
 
 select * from sightings;
+
